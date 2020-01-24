d1bcf4521da9362f2a48520a9929e2d2a3d90b28
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-24 15:20:26 +0900

    Toast Position Changed

--- app/src/main/java/com/gardenlab/sampletoast/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampletoast/MainActivity.java
@@ -3,12 +3,40 @@ package com.gardenlab.sampletoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText1;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
     }
+
+    public void onButton1Clicked(View v) {
+        try {
+            Toast toastView = Toast.makeText(this, "Position Changed", Toast.LENGTH_SHORT);
+
+            int xOffset = Integer.parseInt(editText1.getText().toString());
+            int yOffset = Integer.parseInt(editText2.getText().toString());
+
+            toastView.setGravity(Gravity.TOP|Gravity.TOP, xOffset,yOffset);
+            toastView.show();
+        } catch(NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
